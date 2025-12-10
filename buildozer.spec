76b015afbf3fa45e9244876c3673df87d428525e
管冲 <502187489@qq.com>
2025-12-10 19:07:31 +0800

    gemini version

--- buildozer.spec
+++ buildozer.spec
@@ -1,172 +1,63 @@
 [app]
 
 # (str) Title of your application
-title = Item Recorder
+title = PhotoLog
 
 # (str) Package name
-package.name = itemrecorder
+package.name = photolog
 
 # (str) Package domain (needed for android/ios packaging)
 package.domain = org.test
 
-# (str) Source code where main.py lives
+# (str) Source code where the main.py live
 source.dir = .
 
-# (list) Source files to include (let empty to include all the files in your directory)
+# (list) Source files to include (let empty to include all the files)
 source.include_exts = py,png,jpg,kv,atlas
-
-# (list) List of inclusions using pattern matching
-#source.include_patterns = assets/*,images/*.png
-
-# (list) Source files to exclude (let empty to not exclude anything)
-#source.exclude_exts = spec
-
-# (list) List of directory to exclude (let empty to not exclude anything)
-#source.exclude_dirs = tests, bin
-
-# (list) List of exclusions using pattern matching
-#source.exclude_patterns = license,images/*/*.jpg
 
 # (str) Application versioning (method 1)
 version = 0.1
 
-# (str) Application versioning (method 2)
-# version.regex = __version__ = ['"](.*)['"]
-# version.filename = %(source.dir)s/main.py
-
 # (list) Application requirements
-# comma separated e.g. requirements = sqlite3,kivy
-requirements = python3,kivy
+# 关键点：必须包含 kivymd, pillow (用于图片处理), android
+requirements = python3,kivy==2.2.1,kivymd==1.1.1,pillow,android
 
-# (list) Garden requirements
-#garden_requirements =
+# (str) Presplash of the application
+# presplash.filename = %(source.dir)s/data/presplash.png
 
-# (list) Python-for-android branch to use, defaults to master
-#p4a.branch = master
-
-# (list) Android additional libraries to copy into libs/armeabi
-#android.add_libs_armeabi = libs/android/*.so
-#android.add_libs_armeabi_v7a = libs/android-v7a/*.so
-#android.add_libs_arm64_v8a = libs/android-arm64/*.so
-#android.add_libs_x86 = libs/android-x86/*.so
-#android.add_libs_mips = libs/android-mips/*.so
-
-# (bool) Indicate whether the app should be fullscreen or not
-fullscreen = 0
-
-# (string) Presplash of the application
-#presplash.filename = %(source.dir)s/data/presplash.png
-
-# (string) Icon of the application
+# (str) Icon of the application
 icon.filename = %(source.dir)s/data/icon.png
 
-# (str) Supported orientation (one of landscape, sensorLandscape, portrait or all)
-orientation = portrait
-
 # (list) Permissions
+# 关键点：相机权限和存储权限
 android.permissions = CAMERA,WRITE_EXTERNAL_STORAGE,READ_EXTERNAL_STORAGE
 
 # (int) Target Android API, should be as high as possible.
-android.api = 31
+android.api = 33
 
 # (int) Minimum API your APK will support.
 android.minapi = 21
 
-# (int) Android SDK version to use
-android.sdk = 20
-
 # (str) Android NDK version to use
-android.ndk = 19c
-
-# (int) Android NDK API to use. This is the minimum API your app will support, it should usually match android.minapi.
-android.ndk_api = 21
+android.ndk = 25b
 
 # (bool) Use --private data storage (True) or --dir public storage (False)
-#android.private_storage = True
+android.private_storage = True
 
-# (str) Android NDK directory (if empty, it will be automatically downloaded.)
-#android.ndk_path =
-
-# (str) Android SDK directory (if empty, it will be automatically downloaded.)
-#android.sdk_path =
-
-# (str) ANT directory (if empty, it will be automatically downloaded.)
-#android.ant_path =
-
-# (bool) If True, then skip trying to update the Android sdk
-# This can be useful to avoid excess Internet downloads or save time
-# when an update is due and you just want to test/build your package
-# android.skip_update = False
-
-# (str) Android entry point, default is ok for Kivy apps
-#android.entrypoint = org.renpy.android.PythonActivity
-
-# (list) Pattern to whitelist for the whole project
-#(fnmatch)android.whitelist =
-
-# (str) Path to a custom whitelist file
-#android.whitelist_src =
-
-# (str) Path to a custom blacklist file
-#android.blacklist_src =
+# (str) Android entry point, default is ok for Kivy
+android.entrypoint = org.kivy.android.PythonActivity
 
 # (list) List of Java .jar files to add to the libs so that pyjnius can access
-# their classes. Don't add jars that you do not intend to use
-#android.add_jars = foo.jar
+# android.add_jars = foo.jar,bar.jar
 
 # (list) List of Java files to add to the android project (can be java or a
 # directory containing the files)
-#android.add_src =
+android.add_src =
 
-# (list) Android AAR archives to add (currently works only with sdl2_gradle
-# build)
-#android.add_aars =
+[buildozer]
 
-# (list) Gradle dependencies to add (currently works only with sdl2_gradle
-# build)
-#android.gradle_dependencies =
+# (int) Log level (0 = error only, 1 = info, 2 = debug (with command output))
+log_level = 2
 
-# (list) Java classes to add as activities to the manifest
-#android.add_activities = com.example.Thing
-
-# (str) OUYA Console category. Should be one of GAME or APP
-# If you leave this blank, OUYA support will not be enabled
-#android.ouya.category = GAME
-
-# (str) Filename of OUYA Console icon. It must be a 732x412 png image.
-#android.ouya.icon.filename = %(source.dir)s/data/ouya_icon.png
-
-# (str) XML file to include as an intent filters in <activity> tag
-#android.manifest.intent_filters =
-
-# (str) launchMode to set for the main activity
-#android.manifest.launch_mode = standard
-
-# (list) Android additional libraries to copy into libs/armeabi
-#android.add_libs_armeabi = libs/android/*.so
-#android.add_libs_armeabi_v7a = libs/android-v7a/*.so
-#android.add_libs_arm64_v8a = libs/android-arm64/*.so
-#android.add_libs_x86 = libs/android-x86/*.so
-#android.add_libs_mips = libs/android-mips/*.so
-
-# (bool) Android logcat filters to use
-#android.logcat_filters = *:S python:D
-
-# (bool) Copy library instead of making a libpymodules.so
-#android.copy_libs = 1
-
-# (str) The Android arch to build for, choices: armeabi-v7a, arm64-v8a, x86, x86_64
-android.arch = armeabi-v7a
-
-# (int) overrides automatic versionCode computation
-#android.numeric_version = 1
-
-# (bool) enables generating a 64-bit version of the app
-#android.enable_androidx = False
-
-# (bool) use the new android toolchain (gradle)
-# this value will be set automatically if one of the deployment targets is jelly bean (5) or higher
-#android.gradle = 3
-
-# (str) The format used to package the app for release mode (aab or apk).
-android.release_artifact = apk
+# (int) Display warning if buildozer is run as root (0 = False, 1 = True)
+warn_on_root = 1
--- main.py
+++ main.py
@@ -1,161 +1,169 @@
-from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.label import Label
-from kivy.uix.camera import Camera
-from kivy.uix.image import Image
-from kivy.uix.recycleview import RecycleView
-from kivy.uix.recycleview.views import RecycleDataViewBehavior
-from kivy.uix.behaviors import FocusBehavior
-from kivy.uix.recycleboxlayout import RecycleBoxLayout
-from kivy.properties import BooleanProperty, ListProperty, StringProperty, ObjectProperty
-from kivy.clock import Clock
-from kivy.core.window import Window
 import os
 import json
-from datetime import datetime
 import time
+from datetime import datetime
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
+from kivymd.app import MDApp
+from kivymd.uix.list import TwoLineAvatarIconListItem, IconRightWidget
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivy.utils import platform
 
-# Data storage file
-DATA_FILE = 'items.json'
+# KV 布局语言
+KV = '''
+ScreenManager:
+    HomeScreen:
+    CameraScreen:
 
-class ItemView(RecycleDataViewBehavior, BoxLayout):
-    '''Custom view for each item in the list'''
-    index = None
-    selected = BooleanProperty(False)
-    selectable = BooleanProperty(True)
-    
-    def refresh_view_attrs(self, rv, index, data):
-        self.index = index
-        self.ids['item_image'].source = data['image_path']
-        self.ids['item_time'].text = data['timestamp']
-        return super(ItemView, self).refresh_view_attrs(rv, index, data)
-    
-    def on_touch_down(self, touch):
-        if super(ItemView, self).on_touch_down(touch):
-            return True
-        if self.collide_point(*touch.pos) and self.selectable:
-            return self.parent.select_with_touch(self.index, touch)
-    
-    def apply_selection(self, rv, index, is_selected):
-        self.selected = is_selected
+<HomeScreen>:
+    name: 'home'
+    MDBoxLayout:
+        orientation: 'vertical'
+        
+        MDTopAppBar:
+            title: "物品记录"
+            right_action_items: [["camera", lambda x: app.switch_to_camera()]]
 
-class SelectableRecycleBoxLayout(FocusBehavior, RecycleBoxLayout):
-    '''Adds selection and focus behavior to the view'''
+        MDRecycleView:
+            id: rv
+            viewclass: 'ItemWidget'
+            RecycleBoxLayout:
+                default_size: None, dp(72)
+                default_size_hint: 1, None
+                size_hint_y: None
+                height: self.minimum_height
+                orientation: 'vertical'
 
-class ItemList(RecycleView):
-    def __init__(self, **kwargs):
-        super(ItemList, self).__init__(**kwargs)
-        self.data = []
-        self.load_items()
-    
-    def load_items(self):
-        if os.path.exists(DATA_FILE):
-            with open(DATA_FILE, 'r') as f:
-                items = json.load(f)
-            # Sort by timestamp (newest first)
-            items.sort(key=lambda x: x['timestamp'], reverse=True)
-            self.data = items
-        else:
-            self.data = []
-    
-    def delete_item(self, index):
-        if 0 <= index < len(self.data):
-            item = self.data[index]
-            # Remove image file
-            if os.path.exists(item['image_path']):
-                os.remove(item['image_path'])
-            # Remove from data
-            del self.data[index]
-            # Save updated data
-            with open(DATA_FILE, 'w') as f:
-                json.dump(self.data, f)
-            # Refresh view
-            self.refresh_from_data()
+<CameraScreen>:
+    name: 'camera'
+    MDBoxLayout:
+        orientation: 'vertical'
+        
+        Camera:
+            id: camera
+            resolution: (640, 480)
+            play: False
+            allow_stretch: True
+        
+        MDBoxLayout:
+            size_hint_y: None
+            height: dp(60)
+            padding: dp(10)
+            spacing: dp(10)
+            
+            MDRaisedButton:
+                text: "取消"
+                on_release: app.switch_to_home()
+            
+            MDFillRoundFlatButton:
+                text: "拍照并保存"
+                size_hint_x: 1
+                on_release: app.capture_photo()
+
+<ItemWidget>:
+    IconRightWidget:
+        icon: "trash-can"
+        on_release: root.delete_item(root)
+'''
+
+class ItemWidget(TwoLineAvatarIconListItem):
+    def delete_item(self, widget):
+        # 调用 App 类中的删除方法
+        app = MDApp.get_running_app()
+        app.delete_record(self.text) # text 存储的是时间戳字符串作为ID
+
+class HomeScreen(Screen):
+    pass
 
 class CameraScreen(Screen):
-    def __init__(self, **kwargs):
-        super(CameraScreen, self).__init__(**kwargs)
-        self.camera = Camera(play=True, resolution=(640, 480))
-        self.add_widget(self.camera)
-        
-        # Layout for buttons
-        layout = BoxLayout(orientation='vertical', size_hint=(1, 0.2), pos_hint={'top': 1})
-        capture_btn = Button(text='Capture', size_hint=(1, 0.5))
-        capture_btn.bind(on_press=self.capture)
-        back_btn = Button(text='Back to List', size_hint=(1, 0.5))
-        back_btn.bind(on_press=self.go_back)
-        layout.add_widget(capture_btn)
-        layout.add_widget(back_btn)
-        self.add_widget(layout)
-    
-    def capture(self, instance):
-        # Capture image
-        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        filename = f'item_{int(time.time())}.png'
-        self.camera.export_to_png(filename)
-        
-        # Save to data
-        if os.path.exists(DATA_FILE):
-            with open(DATA_FILE, 'r') as f:
-                items = json.load(f)
-        else:
-            items = []
-        
-        items.append({'image_path': filename, 'timestamp': timestamp})
-        with open(DATA_FILE, 'w') as f:
-            json.dump(items, f)
-        
-        # Go back to list
-        self.manager.current = 'list'
-        self.manager.get_screen('list').ids.item_list.load_items()
-    
-    def go_back(self, instance):
-        self.manager.current = 'list'
+    pass
 
-class ListScreen(Screen):
-    def __init__(self, **kwargs):
-        super(ListScreen, self).__init__(**kwargs)
-        
-        layout = BoxLayout(orientation='vertical')
-        
-        # Button to go to camera
-        camera_btn = Button(text='Take Photo', size_hint=(1, 0.1))
-        camera_btn.bind(on_press=self.go_to_camera)
-        layout.add_widget(camera_btn)
-        
-        # RecycleView for items
-        self.item_list = ItemList()
-        layout.add_widget(self.item_list)
-        
-        # Delete button
-        delete_btn = Button(text='Delete Selected', size_hint=(1, 0.1))
-        delete_btn.bind(on_press=self.delete_selected)
-        layout.add_widget(delete_btn)
-        
-        self.add_widget(layout)
-    
-    def go_to_camera(self, instance):
-        self.manager.current = 'camera'
-    
-    def delete_selected(self, instance):
-        # Find selected items and delete them
-        to_delete = []
-        for i, item in enumerate(self.item_list.data):
-            if hasattr(self.item_list.children[0].children[i], 'selected') and self.item_list.children[0].children[i].selected:
-                to_delete.append(i)
-        
-        # Delete in reverse order to maintain indices
-        for i in sorted(to_delete, reverse=True):
-            self.item_list.delete_item(i)
-
-class ItemApp(App):
+class PhotoLogApp(MDApp):
     def build(self):
-        sm = ScreenManager()
-        sm.add_widget(ListScreen(name='list'))
-        sm.add_widget(CameraScreen(name='camera'))
-        return sm
+        self.title = "Photo Log"
+        # 数据存储路径
+        self.data_dir = self.user_data_dir
+        self.json_file = os.path.join(self.data_dir, 'data.json')
+        self.records = []
+        return Builder.load_string(KV)
+
+    def on_start(self):
+        # 请求 Android 权限 (相机)
+        if platform == 'android':
+            from android.permissions import request_permissions, Permission
+            request_permissions([Permission.CAMERA, Permission.WRITE_EXTERNAL_STORAGE, Permission.READ_EXTERNAL_STORAGE])
+        
+        self.load_records()
+
+    def switch_to_camera(self):
+        self.root.current = 'camera'
+        self.root.get_screen('camera').ids.camera.play = True
+
+    def switch_to_home(self):
+        self.root.get_screen('camera').ids.camera.play = False
+        self.root.current = 'home'
+
+    def capture_photo(self):
+        camera = self.root.get_screen('camera').ids.camera
+        timestr = time.strftime("%Y%m%d_%H%M%S")
+        filename = f"IMG_{timestr}.png"
+        filepath = os.path.join(self.data_dir, filename)
+        
+        # 保存图片
+        camera.export_to_png(filepath)
+        
+        # 保存记录
+        record = {
+            "time_display": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "filename": filename,
+            "filepath": filepath,
+            "id": timestr
+        }
+        self.records.insert(0, record) # 插入到最前面
+        self.save_records()
+        self.refresh_list()
+        self.switch_to_home()
+
+    def delete_record(self, time_display):
+        # 查找并删除
+        record_to_remove = None
+        for rec in self.records:
+            if rec['time_display'] == time_display:
+                record_to_remove = rec
+                break
+        
+        if record_to_remove:
+            # 删除物理文件
+            if os.path.exists(record_to_remove['filepath']):
+                try:
+                    os.remove(record_to_remove['filepath'])
+                except:
+                    pass
+            self.records.remove(record_to_remove)
+            self.save_records()
+            self.refresh_list()
+
+    def load_records(self):
+        if os.path.exists(self.json_file):
+            with open(self.json_file, 'r') as f:
+                self.records = json.load(f)
+        # 按时间倒序排序
+        self.records.sort(key=lambda x: x['id'], reverse=True)
+        self.refresh_list()
+
+    def save_records(self):
+        with open(self.json_file, 'w') as f:
+            json.dump(self.records, f)
+
+    def refresh_list(self):
+        data = []
+        for rec in self.records:
+            data.append({
+                "text": rec['time_display'],
+                "secondary_text": rec['filename']
+            })
+        self.root.get_screen('home').ids.rv.data = data
 
 if __name__ == '__main__':
-    ItemApp().run()
+    PhotoLogApp().run()
